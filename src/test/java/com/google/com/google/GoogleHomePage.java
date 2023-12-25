0bd6ae7df564b3f07bfc34a610474d8330ef073e
Ankur Malviya <madanankurmalviya@gmail.com>
2023-12-25 11:59:55 -0800

    Automatin

---
+++ src/test/java/com/google/com/google/GoogleHomePage.java
@@ -0,0 +1,21 @@
+package com.google.com.google;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+import org.testng.annotations.Test;
+
+public class GoogleHomePage {
+	
+	public static void main(String[] args) {
+		
+	   
+	
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.id("APjFqb")).sendKeys("Home");
+		driver.findElement(By.xpath("//*[contains(text(),'About')]")).click();
+		driver.quit();
+	
+
+}
+}
